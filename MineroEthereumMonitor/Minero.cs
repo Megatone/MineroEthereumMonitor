ba24db86c80c554d3d8d8bf1155d8f353895fc64
Megatone <inacio_pk2@hotmail.com>
2017-08-20 03:06:07 +0200

    comment

--- MineroEthereumMonitor/Minero.cs
+++ MineroEthereumMonitor/Minero.cs
@@ -1,35 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MineroEthereumMonitor
 {
-    class Minero
+    public class Minero
     {
 
-        private int id { get; set; }
-        private string nombre { get; set; }
-        private int gpus { get; set; }
-        private string direccionIP { get; set; }
-        private string hashrate { get; set; }
-        private string token { get; set; }
-        private int timeout { get; set; }
-        private string poolServer1 { get; set; }
-        private int portPoolServer1 { get; set; }
-        private string poolServer2 { get; set; }
-        private int portPoolServer2 { get; set; }
-        private string account { get; set; }
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int gpus { get; set; }
+        public string direccionIP { get; set; }
+        public string hashrate { get; set; }
+        public string token { get; set; }
+        public int timeout { get; set; }
+        public string poolServer1 { get; set; }
+        public int portPoolServer1 { get; set; }
+        public string poolServer2 { get; set; }
+        public int portPoolServer2 { get; set; }
+        public string account { get; set; }
+        public bool minando { get; set; }
 
         public Minero(string _nombre)
         {
+            this.id = 0;
             this.nombre = _nombre;
+            this.gpus = 0;
+            this.direccionIP = GetLocalIPv4(NetworkInterfaceType.Ethernet); 
+            this.hashrate = "0";
+            
             using (MD5 md5Hash = MD5.Create())
             {
                 this.token = GetMd5Hash(md5Hash, string.Concat(Environment.MachineName, Environment.Is64BitOperatingSystem, Environment.Is64BitProcess, Environment.OSVersion, Environment.ProcessorCount, Environment.UserName, Environment.Version));
             }
+
+            this.timeout = 0;
+            this.poolServer1 = string.Empty;
+            this.portPoolServer1 = 0;
+            this.poolServer2 = string.Empty;
+            this.portPoolServer2 = 0;
+            this.account = string.Empty;
         }
 
         private string GetMd5Hash(MD5 md5Hash, string input)
@@ -41,6 +56,25 @@ namespace MineroEthereumMonitor
                 sBuilder.Append(data[i].ToString("x2"));
             }
             return sBuilder.ToString();
+        }
+
+        private string GetLocalIPv4(NetworkInterfaceType _type)
+        {
+            string output = "";
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (item.NetworkInterfaceType == _type && item.OperationalStatus == OperationalStatus.Up)
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+                            output = ip.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return output;
         }
     }
 }
--- MineroEthereumMonitor/Program.cs
+++ MineroEthereumMonitor/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Quobject.Collections.Immutable;
+using Quobject.EngineIoClientDotNet.Client.Transports;
 using Quobject.SocketIoClientDotNet.Client;
 using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace MineroEthereumMonitor
 {
@@ -10,11 +14,14 @@ namespace MineroEthereumMonitor
 
         private static string urlServidor = string.Empty;
         private static Minero minero;
-
+        private static string rutaInicio = AppDomain.CurrentDomain.BaseDirectory;
+ 
         static void Main(string[] args)
         {
-            string help = "Parametros Requqeridos: " + Environment.NewLine + Environment.NewLine + "\t -s <URL SERVIDOR>" + Environment.NewLine + "\t -n <NOMBRE MINERO>" + Environment.NewLine + Environment.NewLine + "Ejemplo : " + Environment.NewLine + Environment.NewLine + "\t MineroEthereumMonitor.exe -s http://localhost:8000 -n Minero1" + Environment.NewLine +  "\t MineroEthereumMonitor.exe -s http://192.168.1.12:8080 -n Minero7";
-           
+          
+            Console.ForegroundColor = ConsoleColor.Green;
+            string help = "Parametros Requqeridos: " + Environment.NewLine + Environment.NewLine + "\t -s <URL SERVIDOR>" + Environment.NewLine + "\t -n <NOMBRE MINERO>" + Environment.NewLine + Environment.NewLine + "Ejemplo : " + Environment.NewLine + Environment.NewLine + "\t MineroEthereumMonitor.exe -s http://localhost:8000 -n Minero1" + Environment.NewLine + "\t MineroEthereumMonitor.exe -s http://192.168.1.12:8080 -n Minero7";
+
             string banner = @" _______  _                               _______       _                                        
 (_______)(_)                             (_______) _   | |                                       
  _  _  _  _  ____   _____   ____  ___     _____  _| |_ | |__   _____   ____  _____  _   _  ____  
@@ -34,41 +41,145 @@ namespace MineroEthereumMonitor
                 }
                 else
                 {
-                    
+
                     Console.WriteLine(help);
                 }
             }
             else
             {
-               
+
                 Console.WriteLine(help);
             }
             while (true)
             {
+                
                 System.Threading.Thread.Sleep(1000);
             }
         }
-
-        private static void iniciarSocket() {
+        static Socket socket;       
+        private static void iniciarSocket()
+        {
             string bar = "############################################################################################################" + Environment.NewLine;
-            var socket = IO.Socket(urlServidor );
-          
+
+            IO.Options options = new IO.Options();
+            options.Transports = ImmutableList.Create(new string[] { WebSocket.NAME, Polling.NAME });
+            socket = IO.Socket(urlServidor, options);
             socket.On(Socket.EVENT_CONNECT, () =>
-            {
-                Console.WriteLine(bar);
-                Console.WriteLine("Conexion establecida con el servidor => " + urlServidor );
-                Console.WriteLine("Sincronizando Información con el servidor...");
-                socket.Emit("inicializarMinero", JObject.Parse(JsonConvert.SerializeObject(minero)));
+            {               
+                    Console.WriteLine(bar);
+                    Console.WriteLine("Conexion establecida con el servidor => " + urlServidor);
+                    Console.WriteLine("Sincronizando Información con el servidor...");
+                    socket.Emit("inicializarMinero", JObject.Parse(JsonConvert.SerializeObject(minero)));               
 
             });
 
-            socket.On(Socket.EVENT_CONNECT_ERROR, () =>{
+            socket.On(Socket.EVENT_CONNECT_ERROR, () =>
+            {
                 Console.WriteLine("ERROR CONECTION");
             });
 
-            socket.On(Socket.EVENT_CONNECT_TIMEOUT, () => {
+            socket.On(Socket.EVENT_CONNECT_TIMEOUT, () =>
+            {
                 Console.WriteLine("TIMEOUT CONECTION");
             });
+
+            socket.On("ConfiguracionMinero", (data) =>
+            {
+                if (!minero.minando)
+                {
+                    minero = JsonConvert.DeserializeObject<Minero>(data.ToString());
+                    Console.WriteLine("Configuración de Minado actualizada" + Environment.NewLine);
+                    Console.WriteLine(bar);
+                    Console.WriteLine("CONFIGURACIÓN :");
+                    Console.WriteLine("PoolSever 1 => " + minero.poolServer1);
+                    Console.WriteLine("Puerto PoolSever 1 => " + minero.portPoolServer1);
+                    Console.WriteLine("PoolSever 2 => " + minero.poolServer2);
+                    Console.WriteLine("Puerto PoolSever 2 => " + minero.portPoolServer2);
+                    Console.WriteLine("Intervalo de Reporte al Panel de Administracion => " + minero.timeout + " ms" + Environment.NewLine);
+                    Console.WriteLine(bar);
+
+                    minar();
+                }
+            });
+
+            socket.On(Socket.EVENT_DISCONNECT, () => {
+                Console.WriteLine("PETO EL SOCKET");
+             
+            });
+
+            socket.On(Socket.EVENT_RECONNECTING, () => {
+                Console.WriteLine("PETO EL SOCKET");
+            
+            });
+            socket.On(Socket.EVENT_RECONNECT, () => {
+                Console.WriteLine("PETO EL SOCKET");
+
+            });
+
+            socket.On(Socket.EVENT_RECONNECT_ATTEMPT, () => {
+                Console.WriteLine("PETO EL SOCKET");
+
+            });
+            socket.On(Socket.EVENT_RECONNECT_ERROR, () => {
+                Console.WriteLine("PETO EL SOCKET");
+
+            });
+            socket.On(Socket.EVENT_RECONNECT_FAILED, () => {
+                Console.WriteLine("PETO EL SOCKET");
+                iniciarSocket();
+
+            });
+            socket.On(Socket.EVENT_ERROR, () => {
+                Console.WriteLine("PETO EL SOCKET");
+
+            });
         }
+
+
+
+        private static void minar()
+        {
+            matarProcesoMinado();
+            Process process = new Process();
+            process.StartInfo.FileName = (string)string.Concat(rutaInicio, @"/ethminer.exe");
+            process.StartInfo.Arguments = (string)string.Concat(" --farm-recheck ", minero.timeout, " -G -S ", minero.poolServer1, ":", minero.portPoolServer1, " -FS ", minero.portPoolServer2, ":", minero.portPoolServer2, " -O ", minero.account, ".", minero.nombre);
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+            process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+            process.Start();
+            minero.minando = true;
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+        }
+
+        private static void matarProcesoMinado()
+        {
+            foreach (var process in Process.GetProcessesByName("ethminer"))
+            {
+                process.Kill();
+            }
+        }
+
+        private static void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            try
+            {
+                Console.WriteLine(outLine.Data);
+                if (outLine.Data.Contains("MH/s"))
+                {
+                    minero.hashrate = outLine.Data.Split(':')[3].Split('M')[0].Trim();                   
+                    socket.Emit("ReporteHashrate", JObject.Parse(JsonConvert.SerializeObject(minero)));
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
     }
 }
